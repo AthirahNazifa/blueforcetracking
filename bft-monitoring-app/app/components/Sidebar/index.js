3f64236e20b180a230f99b00616da02852571e53
Nazifa <athirahnazifa02@gmail.com>
2025-12-05 17:27:29 +0800

    Update Sidebar styles and structure

--- bft-monitoring-app/app/components/Sidebar/index.js
+++ bft-monitoring-app/app/components/Sidebar/index.js
@@ -1,31 +1,39 @@
-//app/components/Sidebar.js
+// app/components/Sidebar.js
 "use client";
-import { FaDesktop, FaPlusCircle, FaSearch, FaVideo } from "react-icons/fa";
+import { FaDesktop, FaSearch, FaVideo } from "react-icons/fa";
 import styles from "./Sidebar.module.css";
 
-export default function Sidebar({ setActivePanel }) {
+export default function Sidebar({ activePanel, setActivePanel }) {
   const menuItems = [
     { name: "Search", icon: <FaSearch size={22} /> },
     { name: "Devices", icon: <FaDesktop size={22} /> },
     { name: "Record Playback", icon: <FaVideo size={22} /> },
-    //{ name: "Add Device", icon: <FaPlusCircle size={22} /> },
   ];
 
   return (
-    <div className={styles.sidebar}>
-      {menuItems.map((item) => (
-        <div key={item.name} className={styles.menuItem}>
-          <button
-            className={styles.button}
-            onClick={() =>
-              setActivePanel((prev) => (prev === item.name ? null : item.name))
-            }
+    <div className={styles.sidebarWrapper}>
+      <div className={styles.sidebar}>
+        {menuItems.map((item) => (
+          <div
+            key={item.name}
+            className={`${styles.menuItem} ${
+              activePanel === item.name ? styles.active : ""
+            }`}
           >
-            {item.icon}
-          </button>
-          <span className={styles.tooltip}>{item.name}</span>
-        </div>
-      ))}
+            <button
+              className={styles.button}
+              onClick={() =>
+                setActivePanel((prev) =>
+                  prev === item.name ? null : item.name
+                )
+              }
+            >
+              {item.icon}
+            </button>
+            <span className={styles.tooltip}>{item.name}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
